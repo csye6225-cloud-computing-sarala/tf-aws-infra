7755d0a9b2fdd3138eaa77dc0e66217a7f3d43cd
saralasharanappa <sharanappakanakagi.s@northeastern.edu>
2024-10-17 11:10:05 -0400

    added security groups

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Main VPC definition
 resource "aws_vpc" "main_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
@@ -80,4 +81,81 @@ resource "aws_route_table_association" "private_association" {
   count          = length(var.private_subnets)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt.id
+}
+
+# Create Security Group for the EC2 Application
+resource "aws_security_group" "app_sg" {
+  name        = "app-security-group"
+  description = "Security group for application server"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000 # Replace with your application's port
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-security-group"
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "app_instance" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public[0].id
+  vpc_security_group_ids      = [aws_security_group.app_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name # Uncommented to allow SSH
+
+  root_block_device {
+    volume_size           = var.volume_size
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "AppInstance"
+  }
+}
+
+# Output the public IP and instance ID of the EC2 instance
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app_instance.public_ip
+}
+
+output "ec2_instance_id" {
+  description = "Instance ID of the EC2 instance"
+  value       = aws_instance.app_instance.id
 }
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "public_subnets" {
 output "private_subnets" {
   value = aws_subnet.private[*].id
 }
+
+# Output the public IP of the EC2 instance
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app_instance.public_ip
+}
+
+# Output the instance ID of the EC2 instance
+output "instance_id" {
+  description = "Instance ID of the EC2 instance"
+  value       = aws_instance.app_instance.id
+}
