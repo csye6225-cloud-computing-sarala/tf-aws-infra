7b73c9c48499168e84766140a5762244d7df1048
saralasharanappa <sharanappakanakagi.s@northeastern.edu>
2024-10-23 23:12:09 -0400

    added user to edit env

--- security_groups.tf
+++ security_groups.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "db_security_group" {
   vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
-    from_port       = 5432 # PostgreSQL port
+    from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
     security_groups = [aws_security_group.app_sg.id] # Allow traffic from the application security group
@@ -61,7 +61,7 @@ resource "aws_security_group" "db_security_group" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_vpc.main_vpc.cidr_block]
   }
 
   tags = {
