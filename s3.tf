3a937ec8015caff5fdf13b7cdac60b3257cd2a2d
saralasharanappa <sharanappakanakagi.s@northeastern.edu>
2024-10-31 02:55:23 -0400

    updated user data

--- s3.tf
+++ s3.tf
@@ -3,12 +3,16 @@ resource "random_uuid" "bucket_name" {}
 
 # S3 Bucket Configuration
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket        = "my-bucket-demo-${random_uuid.bucket_name.result}"
+  bucket        = random_uuid.bucket_name.result
   force_destroy = true
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket_access_block" {
-  bucket = aws_s3_bucket.s3_bucket.id
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # Enable Default Server-Side Encryption
